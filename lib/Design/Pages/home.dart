caf611f02c83033094b1c1fd871104ffdee1bc75
Hazem zammit <hazemzammit1@gmail.com>
2021-12-27 19:41:27 +0100

    menu

--- lib/Design/Pages/home.dart
+++ lib/Design/Pages/home.dart
@@ -1,48 +1,102 @@
 import 'package:application/Design/CustomWidgets/button.dart';
 import 'package:application/Design/Pages/addtripform.dart';
+import 'package:application/Design/Pages/drawermenu.dart';
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget {
-  const home({ Key? key }) : super(key: key);
+class home extends StatefulWidget {
+  
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> with TickerProviderStateMixin{
+  double translateX = 0;
+  double translateY = 0;
+  double scale = 1;
+  bool toggle = false ;
+  late AnimationController _animationController ;
+
+  @override
+  void initState() {
+    _animationController =AnimationController(vsync: this, duration: Duration(milliseconds:300 ) );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Image.asset('images/grid.png'),
-        title: Text("Camping Trips" ,style: TextStyle(color: Colors.black , fontSize: 20,),),
-        centerTitle: true,
-        actions: [Container(
-          width: 50,
-          child:Image.asset('images/bell.png'),
-        )],
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox( height: 20, ),
-              idbox(),
-              SizedBox( height: 90, ),
-              Image.asset('images/Illustration.png'),
-              SizedBox( height: 30, ),
-              Text("You didn’t add any trips before.", style: TextStyle(color:Color(0xFF3F3F46) , fontSize: 16),),
-              SizedBox( height: 130, ),
+    return Stack(
+      children: [
+        drawermenu(),
+        AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          transform: Matrix4.translationValues(translateX, translateY, 0)..scale(scale),
+          child: ClipRRect(
+            borderRadius:(toggle)? BorderRadius.circular(20): BorderRadius.circular(0),
+            child: Scaffold(
+              appBar: AppBar(
+                leading:IconButton(
+                  icon: AnimatedIcon(icon: AnimatedIcons.menu_arrow,
+                  color:Colors.black,
+                  progress: _animationController),
 
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => addtripform()),
-                  );
-                },
-                child: buttondefault(title: 'Start New Trip '),
-              ),
-            ],
+                  onPressed: (){
+                    toggle= !toggle;
+                    if(toggle){
+                      translateX = 200 ;
+                      translateY= 80 ;
+                      scale=0.8;
+                      _animationController.forward();
+                    } else{
+                      translateX = 0 ;
+                      translateY= 0 ;
+                      scale=1;
+                      _animationController.reverse();
+                    }
+                    setState(() {
+                    });
+                  },
+
+                ),
+
+                
+                title: Text("Camping Trips" ,style: TextStyle(color: Colors.black , fontSize: 20,),),
+                centerTitle: true,
+                actions: [Container(
+                  width: 50,
+                  child:Image.asset('images/bell.png'),
+                )],
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                ),
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox( height: 20, ),
+                      idbox(),
+                      SizedBox( height: 90, ),
+                      Image.asset('images/Illustration.png'),
+                      SizedBox( height: 30, ),
+                      Text("You didn’t add any trips before.", style: TextStyle(color:Color(0xFF3F3F46) , fontSize: 16),),
+                      SizedBox( height: 130, ),
+                  
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => addtripform()),
+                          );
+                        },
+                        child: buttondefault(title: 'Start New Trip '),
+                      ),
+                    ],
+                  ),
+                ),
+            ),
           ),
         ),
+        
+      ],
     );
   }
 }
